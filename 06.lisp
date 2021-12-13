8292df9a47a54c01f95b57a5a38aba28c29bc199
Wes Florence <hello@wesflorence.com>
2021-12-13 01:06:08 -0800

    Hashmap solution
    I banged my head for a while trying to get a more "mathematical"
    solution, since the pattern is predictable. Seems like something that
    could be solved much more elgantly. But this dp-ish solution using a
    hash map was fast enough!

--- 06.lisp
+++ 06.lisp
@@ -1,4 +1,5 @@
 (ql:quickload :cl-ppcre)
+
 (defun read-input (file)
   (with-open-file (in file)
     (loop
@@ -7,24 +8,34 @@
       collect (mapcar #'parse-integer
                       (cl-ppcre:all-matches-as-strings "(\\d+)" line)))))
 
-(defparameter *population* (car (read-input "06.input")))
-
-(defun spawn (population)
-  (loop for fish in population
-        if (equal fish 0) collect 8))
-
 (defun age (population)
-  (mapcar (lambda (fish)
-            (if (equal fish 0) 6 (- fish 1)))
-          population))
+  (let ((new-generation (make-hash-table)))
+    (maphash (lambda (fish-state fish-count)
+               (if (equal 0 fish-state)
+                 (progn
+                   (incf (gethash 6 new-generation 0) fish-count)  ;; current fish is now 6
+                   (incf (gethash 8 new-generation 0) fish-count)) ;; new fish age 8
+                 (incf (gethash (1- fish-state) new-generation 0) fish-count))) ;; next gen is all 1 year younger
+             population)
+    new-generation))
 
-(defun tick (population)
-  (append (age population)
-          (spawn population)))
+(defun build-initial-generaiton (input)
+  (let ((input (car (read-input input)))
+        (population (make-hash-table)))
+    (loop for fish-state in input
+          do (incf (gethash fish-state population 0)))
+    population))
 
-(defun simulate (cycles population)
-  (loop for i from 1 to cycles
-        do (setf population (tick population))
-        finally (return population)))
+(defun simulate (days input)
+  (let ((population (build-initial-generaiton input)))
+    (loop for day from 1 to days
+          do (setf population (age population))
+          finally (return population))))
 
-(length (simulate 80 *population*))
+(defun simulate-sum-fish (days input)
+  (loop for fish-state being the hash-key
+          using (hash-value spawn-count) of (simulate days input)
+        summing spawn-count))
+
+(format t "Problem 06 A: ~a~%" (simulate-sum-fish 80 "06.input"))
+(format t "Problem 06 B: ~a~%" (simulate-sum-fish 256 "06.input"))
